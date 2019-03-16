5fec1cb05b878622abaac063e34f672110bfde2e
Braised-Cakes <1930003003@qq.com>
2019-03-16 09:31:30 +0800

    update

--- README.md
+++ README.md
@@ -1,5 +1,36 @@
-## IMAGESLIM
+# `imageslim`
+
+[![npm](https://img.shields.io/npm/v/imageslim.svg?style=flat-square)](https://npmjs.org/imageslim)
+Elegant terminal spinner
+
+## Install
+
+With `npm`:
 
 ```
-使用方法：
+npm install imageslim -g
+```
+
+
+## Usage
+
+```
+imageslim [option] [in_file]
+```
+Compressed images will be recorded in`.imageslimrc` files and will not be compressed repeatedly.
+
+
+## Example
+```
+/* Compress all images in the current directory and overwrite the original file */
+imageslim 		
+
+/* Compress all pictures in the specified directory and overwrite the original file */
+imageslim images
+
+/* Support for generating webp format pictures  */
+imageslim images -w
+
+/* Force compression of images even though they have already been compressed */
+imageslim images -f
 ```
--- index.js
+++ index.js
@@ -1,93 +1,88 @@
 #!/usr/bin/env node
 
-/**
- * 使用方法： imageslim
- * 
- * cli形式调用  imageslim -wf source output 
- * api形式调用  
- * 
- * 
- * imageslim 就是压缩当前目录       没问题
- * imageslim source 压缩指定目录    
- * imageslim source output 把a目录的图片压缩到b目录
- * 
- * 
- * 规则：
- * 1. 如果从a目录压缩到b目录，不生成记录
- * 2. 同目录下压缩，默认生成记录
- * 3. 支持记录图片压缩信息，不重复压缩 TODO
- * 
- * TODO：目前要求必须是目录
- * 
- * 判断是否同一个目录
- */
-const program = require('commander');
-const imagemin = require('imagemin');
-const package = require('./package.json');
-const imageminMozjpeg = require('imagemin-mozjpeg');
-const imageminPngquant = require('imagemin-pngquant');
-const imageminWebp = require('imagemin-webp');
-const glob = require('glob');
-const chalk = require('chalk');
-const md5 = require('md5');
-const path = require('path');
-const fs = require('fs');
-const log = console.log;
+const program = require('commander')
+const imagemin = require('imagemin')
+const package = require('./package.json')
+const imageminMozjpeg = require('imagemin-mozjpeg')
+const imageminPngquant = require('imagemin-pngquant')
+const imageminWebp = require('imagemin-webp')
+const glob = require('glob')
+const chalk = require('chalk')
+const md5 = require('md5')
+const ProgressBar = require('progress')
+const path = require('path')
+const fs = require('fs')
+const log = console.log
 const cacheFilePath = path.resolve(process.cwd(), '.imageslimrc') //图片压缩信息存储路径
 
+if (!fs.existsSync(cacheFilePath)) {
+    fs.writeFileSync(cacheFilePath, '', 'utf-8')
+}
 
-
+const cacheList = fs.readFileSync(cacheFilePath, 'utf-8').trim().split('\n')
 program
     .version(package.version)
     .usage('imageslim [option] in_file [-o out_file]')
     .description('imageslim images')
     .option('-w --webp', 'convert (jpg|png|jpeg) to webp')
-    .option('-o --output', 'convert (jpg|png|jpeg) to webp')
     .option('-f --force', 'ignore md5')
-    .parse(process.argv);
+    .parse(process.argv)
 
-let [source, target] = program.args;
+let [source] = program.args
 if (!source) {
-    source = process.cwd();
+    source = process.cwd()
 }
-
 let imgs = glob.sync(path.resolve(source, '**/*.@(jpg|jpeg|png)'), {
     ignore: [path.resolve(process.cwd(), 'node_modules/**')]
-});
+})
 
 imgs = imgs.map((item) => {
     return {
         source: item,
-        target: target ? target : path.parse(item).dir
+        target: path.parse(item).dir
     }
-});
-console.log(imgs);
-// return;
+})
+
+// 非强制压缩，匹配缓存数据进行过滤
+if (!program.force) {
+    imgs = imgs.filter((item) => {
+        return !cacheList.includes(md5(fs.readFileSync(item.source)))
+    })
+}
+
+;
 (async () => {
+    log('')
+    log(`${chalk.green('✈️')}  imageslim started`)
+    let bar = new ProgressBar('[:bar] :percent  :current/:total', {
+        total: imgs.length,
+        width: 100
+    })
     for (let i = 0; i < imgs.length; i++) {
-        let item = imgs[i];
+        let item = imgs[i]
         if (['.jpg', '.jpeg'].includes(path.parse(item.source).ext)) {
             await imagemin([item.source], item.target, {
                 use: [
                     imageminMozjpeg()
                 ]
-            });
+            })
         } else if (['.png'].includes(path.parse(item.source).ext)) {
             await imagemin([item.source], item.target, {
                 use: [
                     imageminPngquant()
                 ]
-            });
+            })
         }
         await imagemin([item.source], item.target, {
             use: [
                 imageminWebp()
             ]
-        });
+        })
+        bar.tick()
     }
     for (let i = 0; i < imgs.length; i++) {
-        console.log(md5(fs.readFileSync(imgs[i])))
+        fs.appendFileSync(cacheFilePath, '\n' + md5(fs.readFileSync(imgs[i].source)))
     }
-    /* 记录图片压缩信息 TODO*/
-    log(`${chalk.green('✔')}  imageslim webp succeed`)
-})();
+    log(`${chalk.green('✔')}  imageslim succeed`)
+    log('')
+})()
